97cb6da619499ea1b489ba88c3739ea65f48e992
Hernany Simao <hernanysimao123@gmail.com>
2024-08-18 01:47:53 +0100

    Controller: Get with id params

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { get } from 'http';
 
 @Controller('courses')
 export class CoursesController {
 @Get()
 findAll(){
     return "Listando os cursos"
+}
+
+@Get(':id')
+findOne(@Param() params){
+  return `Buscando o curso com ID: ${params.id}`
 }
 }
