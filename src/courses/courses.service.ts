299d15009795c56e56a144f6d5393a0231b9900e
Hernany Simao <hernanysimao123@gmail.com>
2024-08-18 03:04:14 +0100

    Service - CRUD done

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -8,4 +8,34 @@ export class CoursesService {
         name: "Curso de Nestjs",
         tags: ['NodeJs']
     }]
+
+    findAll(){
+    return this.courses;
+    }
+
+    findOne(id: number){
+     return this.courses.find(course => course.id === id)
+    }
+
+    create(createCoursesDTO: any){
+    this.courses.push(createCoursesDTO)
+    }
+
+    update(id: number, updateCoursesDTO: any){
+        const existingCourse = this.findOne(id)
+        if(existingCourse){
+            const idx = this.courses.findIndex(course=> course.id === id)
+            this.courses[idx] = {
+                id,
+                ...updateCoursesDTO
+            }
+        }
+    }
+
+    remove(id: number){
+        const idx = this.courses.findIndex(course=> course.id === id)
+        if(idx >=0){
+            this.courses.splice(idx, 1)
+        }
+    }
 }
