d65ec8a364c74a28a649db32cd2311017dcff3e3
Hernany Simao <hernanysimao123@gmail.com>
2024-08-19 02:31:05 +0100

    new migration

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,17 @@
-import { DataSource } from "typeorm";
-import { dataSourceOptions } from "./database.module";
-import { CreateCoursesTable1724026150293 } from "src/migrations/1724026150293-CreateCoursesTable";
-import { CreateTagsTable1724027590350 } from "src/migrations/1724027590350-CreateTagsTable";
+import { DataSource } from 'typeorm';
+import { dataSourceOptions } from './database.module';
+import { CreateCoursesTable1724026150293 } from 'src/migrations/1724026150293-CreateCoursesTable';
+import { CreateTagsTable1724027590350 } from 'src/migrations/1724027590350-CreateTagsTable';
+import { CreateCoursesTagsTable1724028880712 } from 'src/migrations/1724028880712-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1724030155448 } from 'src/migrations/1724030155448-AddCoursesIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
-    ...dataSourceOptions,
-    synchronize: false,
-    migrations: [CreateCoursesTable1724026150293, CreateTagsTable1724027590350],
-})
+  ...dataSourceOptions,
+  synchronize: false,
+  migrations: [
+    CreateCoursesTable1724026150293,
+    CreateTagsTable1724027590350,
+    CreateCoursesTagsTable1724028880712,
+    AddCoursesIdToCoursesTagsTable1724030155448,
+  ],
+});
